0ee6480dfe97de6302e6ac401898025dcf62b0c6
Antonio Marinho <72404175+antmarinho@users.noreply.github.com>
2023-10-16 13:51:19 -0300

    Add files via upload

---
+++ Java/Exercicios-jpa/src/main/java/teste/ObterUsuario.java
@@ -0,0 +1,25 @@
+package teste;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.Usuario;
+
+public class ObterUsuario {
+	
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Exercicios-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		//pesquisa por id
+		Usuario user = em.find(Usuario.class,1L);
+		
+		System.out.println(user.getNome());
+		
+		emf.close();
+		em.close();
+	}
+
+}
