9ffa008c234f27596afadea1b30cb306a9c71a62
Antonio Marinho <72404175+antmarinho@users.noreply.github.com>
2023-11-07 12:38:47 -0300

    Add files via upload

--- Java/ExercicioSpring/src/main/java/com/acgm/controllers/ClienteController.java
+++ Java/ExercicioSpring/src/main/java/com/acgm/controllers/ClienteController.java
@@ -1,7 +1,9 @@
 package com.acgm.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.acgm.models.Cliente;
@@ -14,6 +16,20 @@ public class ClienteController {
 	public Cliente obterCliente() {
 		
 		return new Cliente(28,"jose","111-555-999-85");
+		
+	}
+	
+	@GetMapping("/{id}")
+	public Cliente obterClienteId1(@PathVariable int id) {
+		
+		return new Cliente(id,"maria","578-956-472-40");
+		
+	}
+	
+	@GetMapping
+	public Cliente obterClienteId2(@RequestParam(name = "id") int id) {
+		
+		return new Cliente(id,"jose","452-321-785-56");
 		
 	}
 
