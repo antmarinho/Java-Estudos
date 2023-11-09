d92e8ed5a7b2c5f9bba3accf77ad79c4771c7ab4
Antonio Marinho <72404175+antmarinho@users.noreply.github.com>
2023-11-09 11:36:42 -0300

    Add files via upload

--- Java/ExercicioSpring/src/main/java/com/acgm/controllers/ProdutoController.java
+++ Java/ExercicioSpring/src/main/java/com/acgm/controllers/ProdutoController.java
@@ -1,8 +1,19 @@
 package com.acgm.controllers;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +27,62 @@ public class ProdutoController {
 	@Autowired
 	private ProdutosRepository prodRepository;
 	
-	@PostMapping()
-	public @ResponseBody Produto novoProduto(Produto prod) {
+	//@PostMapping
+	// inserir e editar
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
+	public @ResponseBody Produto salvarProdutoa(@Valid Produto prod) {
 		
 		prodRepository.save(prod);
 				
 		return prod;
+		
+	}
+	
+	@GetMapping
+	public Iterable<Produto> obterProdutos() {
+		
+		return prodRepository.findAll();
+	}
+	
+	@GetMapping(path = "/nome/{parteNome}")
+	public Iterable<Produto> obterProdutosPorNome(@PathVariable String parteNome) {
+		
+		return prodRepository.searchByNameLike(parteNome);
+		
+		//return prodRepository.findByNomeContainingIgnoreCase(parteNome);
+	}
+	
+	//consulta paginada
+	@GetMapping(path = "/pagina/{numeroPagina}/{qtdPagina}")
+	public Iterable<Produto> obterProdutosPorPagina(@PathVariable int numeroPagina, @PathVariable int qtdPagina) {
+		
+		if(qtdPagina >= 5)
+			qtdPagina = 5;
+		
+		Pageable page = PageRequest.of(numeroPagina, qtdPagina);
+		
+		return prodRepository.findAll(page);
+	}
+	
+	@GetMapping(path = "/{id}")
+	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
+		
+		return prodRepository.findById(id);
+	}
+	
+	/*@PutMapping editar separado
+	public Produto alterarProduto(@Valid Produto prod) {
+		
+		prodRepository.save(prod);
+		
+		return prod;
+		
+	}*/
+	
+	@DeleteMapping(path = "/{id}")
+	public void excluirProduto(@PathVariable int id) {
+		
+		prodRepository.deleteById(id);
 		
 	}
 
--- Java/ExercicioSpring/src/main/java/com/acgm/model/entities/repositories/ProdutosRepository.java
+++ Java/ExercicioSpring/src/main/java/com/acgm/model/entities/repositories/ProdutosRepository.java
@@ -1,10 +1,30 @@
 package com.acgm.model.entities.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
 import com.acgm.model.entities.Produto;
 
-public interface ProdutosRepository extends CrudRepository<Produto,Integer> {
+//public interface ProdutosRepository extends CrudRepository<Produto,Integer> {
 	
-	
+public interface ProdutosRepository extends PagingAndSortingRepository<Produto,Integer> {	
 
+	//o spring vai implementar sozinho
+	public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome);
+	
+	//findByNomeContaining
+	//findByNomeIsContaining
+	//findByNomeContains
+	
+	//faz a msm coisa
+	
+	//findByNomeStartsWith    
+	//findByNomeEndsWith
+	//findByNomeNotContaining
+	
+	@Query("SELECT p FROM Produto p WHERE p.nome LIKE %:nome%")
+	public Iterable<Produto> searchByNameLike(@Param("nome") String nome);
+	
 }
